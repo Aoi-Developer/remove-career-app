8ba519b90d0aba680a48df854f78bbce09b0b8dc
竹林人間 <lzuoteng99@gmail.com>
2022-05-01 10:02:59 +0900

    Add files via upload

---
+++ appremove.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+adb shell pm list package >> pkg.txt
+cat pkg.txt | sed -e "s/package://g" | grep 'docomo' | sed "s/^/adb shell pm uninstall -k --user 0  /g" >> rm.sh
+cat pkg.txt | sed -e "s/package://g" | grep 'ntt' | sed "s/^/adb shell pm uninstall -k --user 0  /g" >> rm.sh
+#cat pkg.txt | sed -e "s/package://g" | grep 'au' | sed "s/^/adb shell pm uninstall -k --user 0  /g" >> rm.sh
+cat pkg.txt | sed -e "s/package://g" | grep 'rakuten' | sed "s/^/adb shell pm uninstall -k --user 0  /g" >> rm.sh
+cat pkg.txt | sed -e "s/package://g" | grep 'kddi' | sed "s/^/adb shell pm uninstall -k --user 0  /g" >> rm.sh
+cat pkg.txt | sed -e "s/package://g" | grep 'softbank' | sed "s/^/adb shell pm uninstall -k --user 0  /g" >> rm.sh
+
+cat rm.sh
+
+echo -n "以上のアプリが消去されます [Y/n]: "
+read ANS
+
+case $ANS in
+  "" | [Yy]* )
+    sh rm.sh
+    rm rm.sh pkg.txt
+    echo "アプリの消去を実行しました"
+    ;;
+  * )
+    rm rm.sh pkg.txt
+    echo "処理を中止しました。"
+    ;;
+esac
+
